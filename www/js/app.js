3cc7f1841dbd62311a5532ad48718e273da0eedf
Benes <benes@kodarna.cz>
2014-01-06 10:42:14 +0100

    lang

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,8 @@
-angular.module('App', ['ngRoute', 'App.services', 'App.controllers'])
+angular.module('App', ['ngRoute', 'App.services', 'App.controllers', 'pascalprecht.translate'])
     // .config(['$compileProvider', function ($compileProvider) {
     //     $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
     // }])
-    .config(function ($routeProvider) {
+    .config(function ($routeProvider, $translateProvider) {
         $routeProvider
         .when('/', {
             controller: 'MainCtrl',
@@ -13,4 +13,20 @@ angular.module('App', ['ngRoute', 'App.services', 'App.controllers'])
             templateUrl: 'partials/view.html'
         })
         .otherwise({redirectTo: '/'});
+
+        $translateProvider.translations('en', {
+            'TITLE': 'Hello',
+            'FOO': 'This is a paragraph',
+            'main': 'Main section',
+            'view': 'view section'
+        });
+
+        $translateProvider.translations('de', {
+            'TITLE': 'Hallo',
+            'FOO': 'Dies ist ein Paragraph de',
+            'main': 'Main section de',
+            'view': 'view section de'
+        });
+
+        $translateProvider.preferredLanguage('en');
     });
